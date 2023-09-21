be5827d4ac931494c2a4b11e297065f32847d469
marcmata26 <marcmata26@gmail.com>
2023-09-21 16:52:54 +0000

    updates to logic

--- swapi-search-assisted.sh
+++ swapi-search-assisted.sh
@@ -26,23 +26,22 @@ case $opt in
 		# Query API
 		api_url="https://swapi.dev/api/$opt/?search=$searchTerm"
 		api_url=$(echo $api_url | sed 's/ /%20/g')
-		echo -e "\033[31m$api_url\033[0m"
 		response=$(curl -s "$api_url")
 
-		# Echo if successful
+		# Iterate through sub-URLs if successful
 		if [ $? -eq 0 ]
 		then
-			echo "$response" | jq
-		
-		# Expand URLs of result
-            expanded_urls=$(echo "$response" | jq -r '.results[].url')
-            
-            # for i in ${!options[@]}
-            # do
-			# 	option=${!options[i]}
-            #     echo $response | jq ".results[].$option.url"
-            # done
-		
+
+            for option in ${options[@]}
+            do
+                echo $response | jq -r ".results[].$option[]" 2> /dev/null | while read -r api_url_sub
+				do
+                    responseSub=$(curl -s $api_url_sub)
+					title=$(echo "$responseSub" | jq -r '.title')
+					name=$(echo "$responseSub" | jq -r '.name')
+				done
+            done
+
 		else
 			echo "Error: Failed to retrieve Star Wars data"
 		fi
